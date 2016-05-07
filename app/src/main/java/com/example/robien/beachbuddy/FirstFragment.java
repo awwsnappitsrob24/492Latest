fea12ee0e5ff6d0cca82eca7ff109cbd3e2df7a4
MKridler <kridler240@gmail.com>
2016-05-06 18:42:53 -0700

    tutorial update
    replaced imageview with webview and hosted image on our server

--- app/src/main/java/com/example/robien/beachbuddy/FirstFragment.java
+++ app/src/main/java/com/example/robien/beachbuddy/FirstFragment.java
@@ -3,13 +3,20 @@ package com.example.robien.beachbuddy;
 /**
  * Created by Robien on 5/4/2016.
  */
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.InputStream;
+import java.net.URL;
 
 public class FirstFragment extends Fragment {
 
@@ -28,10 +35,17 @@ public class FirstFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         View v = inflater.inflate(R.layout.first_frag_layout, container, false);
 
         TextView textView = (TextView) v.findViewById(R.id.tvFragFirst);
         textView.setText(getArguments().getString("msg"));
+        textView.setVisibility(View.INVISIBLE);
+        WebView web = (WebView) v.findViewById(R.id.loginProfileImage);
+        web.loadUrl("http://52.25.144.228/home2.png");
+        web.getSettings().setLoadWithOverviewMode(true);
+        web.getSettings().setUseWideViewPort(true);
+
 
         return v;
     }
